ebc587a4f7b777f1003d6cf015c9acefaad76023
fatumankhangaa <fatumankhangaa's-email@gmail.com>
2026-06-26 15:21:00 +0300

    Updated backend server

--- src/app.js
+++ src/app.js
@@ -15,7 +15,13 @@ const trashRoutes = require("./routes/trash.routes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors(
+    {
+      origin: "*",
+      credentials: true
+    }
+  ));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use("/api/admin", adminRoutes);
